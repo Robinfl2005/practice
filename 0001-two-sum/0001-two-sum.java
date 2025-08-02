476f624dc6a23cbf833b52a64ca22f92d56adce9
Robinfl2005 <165790676+Robinfl2005@users.noreply.github.com>
2025-08-02 15:25:23 +0530

    Time: 46 ms (16.02%), Space: 44.9 MB (79.03%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public static void main(String[]args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int nums[]=new int[n];
+        for(int i=0;i<nums.length;i++) {
+            nums[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        System.out.print(nums);
+    }
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++) {
+            for(int j=i+1;j<nums.length;j++) {
+                if(nums[i]+nums[j]==target)
+                return new int[]{i,j};
+                
+            }
+        }
+        return new int[]{};
+    }
+}

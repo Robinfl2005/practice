8698a7203aecdb7efbb61ccc141797f81f0652f4
Robinfl2005 <165790676+Robinfl2005@users.noreply.github.com>
2025-08-27 23:12:23 +0530

    Time: 35 ms (88.18%), Space: 53 MB (79.35%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(other) {
+            if(val!==other) throw "Not Equal";
+            return true;
+        },
+        notToBe: function(other) {
+            if(val===other) throw "Equal";
+            return true;
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
